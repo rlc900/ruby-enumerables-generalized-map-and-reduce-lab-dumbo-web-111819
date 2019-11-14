25674aed75f2758c3c897d23deb56f7da6ae86e6
Rianna Cleary <rianna.cleary@gmail.com>
2019-11-14 16:47:08 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,13 @@
-# Your Code Here
+def map(array)
+  new_array = []
+  i = 0 
+  while i < array.length 
+  new_array.push(yield(array[i]))
+  i += 1 
+  end
+  new_array
+end 
+
+def reduce(array, starting_point = 0)
+  
+end 
